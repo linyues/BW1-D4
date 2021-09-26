64a6f04b3724496f34c96770a01330eadb98e3bb
linyues <2694166721@qq.com>
2021-09-26 22:48:32 +0800

    CollectionOperator and Filter finished

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,30 +2,74 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        if (left < right) {
+            for (int i = left; i <= right; i++) {
+                result.add(i);
+
+            }
+        } else {
+            for (int i = left; i >= right; i--) {
+                result.add(i);
+            }
+        }
+
+        return result;
+
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        if (left < right) {
+            for (int i = left; i <= right; i++) {
+                if (i % 2 == 0) {
+                    result.add(i);
+                }
+
+
+            }
+        } else {
+            for (int i = left; i >= right; i--) {
+                if(i % 2 == 0){
+                    result.add(i);
+                }
+
+            }
+        }
+
+        return result;
+
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream(array).boxed().filter(elem -> elem % 2 == 0).collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return array[array.length-1];
+
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> sec = Arrays.stream(secondArray).boxed().collect(Collectors.toList());
+        return Arrays.stream(firstArray).boxed().filter(elem -> sec.contains(elem)).collect(Collectors.toList());
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> sec = Arrays.stream(secondArray).collect(Collectors.toList());
+        List<Integer> fir = Arrays.stream(firstArray).collect(Collectors.toList());
+        fir.addAll(sec);
+        return fir.stream().distinct().collect(Collectors.toList());
+
     }
+
 }
